d439a01afb15990a99d7eb5dfe2405ba24e923cd
Zoxemik <166174663+Zoxemik@users.noreply.github.com>
2024-11-04 15:18:24 +0100

    PlayerSingedInotEaNation done?

--- src/Skateboard3Server.Web/Services/Models/SkateFeed/PlayerSignedIntoEaNation.cs
+++ src/Skateboard3Server.Web/Services/Models/SkateFeed/PlayerSignedIntoEaNation.cs
@@ -1,6 +1,10 @@
-﻿namespace Skateboard3Server.Web.Services.Models.SkateFeed;
+﻿using Skateboard3Server.Web.Services.Models.Common;
+
+namespace Skateboard3Server.Web.Services.Models.SkateFeed;
 
 public class PlayerSignedIntoEaNation
 {
     //TODO: add properties
+    public PlatformType PlatformId { get; set; }
+    public uint UserId { get; set; }
 }
